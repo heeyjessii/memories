fdf75e1f476bdfbf78db55d1b2e9d837d9bbf08f
Jessica <11lopez.jessica@gmail.com>
2017-02-03 09:12:35 -1000

    it can finally flip

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
-function onClick () {
-  document.getElementById('flex').style.transform = 'rotateX(180deg)';
+function flip(id) {
+  document.getElementById(id).style.transform = 'rotateY(180deg)';
+  if(document.getElementById(id).style.transform === 'rotateY(180deg)'){
+    setTimeout(function(){
+      document.getElementById(id).style.transform = "rotateY(0deg)";
+    }, 1000);
+  }
 }
-
-click();
