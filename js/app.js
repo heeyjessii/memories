c5946a8f484f03329b4dc766e8e6e82532bccb37
11lopez.jessica@gmail.com <11lopezjessica@gmail.com>
2017-02-13 09:52:27 -1000

    added what I got

--- js/app.js
+++ js/app.js
@@ -1,28 +1,39 @@
 var clicks = 0;
 var firstClick;
 var secondClick;
+var cardBack = "./assets/download.png"
+var matches = 0;
 
 function flip(id) {
-  console.log(document.getElementById(id).getElementsByTagName('img')[0].src);
   document.getElementById(id).style.transform = 'rotateY(180deg)';
   if(document.getElementById(id).style.transform === 'rotateY(180deg)'){
     setTimeout(function(){
       document.getElementById(id).style.transform = "rotateY(0deg)";
     }, 1000);
   }
-  if(flicks === 2) {
-    return;
+  if(clicks === 2) {
   }
   if (clicks === 0) {
-    firstClick = card;
+    firstClick = id;
     clicks = 1;
   } else {
     clicks = 2;
-    secondClick = card;
-    timer = setInterval("check()", 1000)
+    secondClick = id;
+    timer = setInterval("check()", 1000);
   }
 }
-//when card is clicked it turns for a while
+
+function check() {
+  clearInterval(timer);
+  clicks = 0;
+  if (document.getElementById('eight') == document.getElementById('three')) {
+    document.getElementById('eight').style.opacity = "0";
+    document.getElementById('three').style.opacity = "0";
+  } else {
+    document.firstClick = cardBack;
+    document.secondClick = cardBack;
+  }
+}
 
 
 
